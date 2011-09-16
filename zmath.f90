9db477d5d8ff3d292fbcd0daecc97e4f50ab965f
elseifkk <elseifkk@a4474769-b17b-4e2e-aef2-be6ca77db40a>
2011-09-16 08:39:16 +0000

    bug fix. gamma function implemented: first try.

--- zmath.f90
+++ zmath.f90
@@ -15,6 +15,7 @@ module zmath
   public zm_nop
   public zm_neg
   public zm_fac
+  public zm_dfac
 
   public zm_sin
   public zm_cos
@@ -41,10 +42,14 @@ module zmath
   public zm_imag
   public zm_mag
   public zm_arg
+  public zm_gamma
+  public zm_lgamma
 
   public zm_min
   public zm_max
 
+  real(rp),parameter::pi =4.0_rp*atan(1.0_rp)
+  real(rp),parameter::pi2=2.0_rp*pi
 contains
 
   logical function is_integer(z,n)
@@ -130,12 +135,29 @@ contains
     complex(cp),intent(in)::z1
     integer i,n
     n=int(z1)
-    zm_fac=1.0_rp
-    if(n<=1) return
-    do i=2,n
+    if(n<=0) then
+       zm_fac=czero
+       return
+    end if
+    zm_fac=real(n,kind=rp)       
+    do i=2,n-1
        zm_fac=zm_fac*real(i,kind=rp)
     end do
   end function zm_fac
+
+  complex(cp) function zm_dfac(z1)
+    complex(cp),intent(in)::z1
+    integer i,n
+    n=int(z1)
+    if(n<=0) then
+       zm_dfac=czero
+       return
+    end if
+    zm_dfac=real(n,kind=rp)
+    do i=n-2,2,-2
+       zm_dfac=zm_dfac*real(i,kind=rp)
+    end do
+  end function zm_dfac
 
   complex(cp) function zm_sin(z1)
     complex(cp),intent(in)::z1
@@ -304,5 +326,58 @@ contains
        end if
     end if
   end function zm_max
+
+  complex(cp) function zm_lgamma(z1)
+    complex(cp),intent(in)::z1
+    real(rp),parameter::a(7)=[&
+         1.0_rp/12.0_rp,&
+         1.0_rp/30.0_rp,&
+         53.0_rp/210.0_rp,&
+         195.0_rp/371.0_rp,&
+         22999.0_rp/22737.0_rp,&
+         29944523.0_rp/19733142.0_rp,&
+         109535241009.0_rp/48264275462.0_rp]
+    real(rp),parameter::log_pi2_2=log(pi2)*0.5_rp
+    complex(cp) z,d,r
+    real(rp) r1
+    integer i
+    integer n
+    if(imagpart(z1)==0.0_rp) then
+       zm_lgamma=lgamma(realpart(z1))
+       return
+    end if
+    r1=realpart(z1)
+    if(r1<rzero) then
+       n=-int(r1)+1
+       z=z1+real(n,kind=rp)
+    else
+       n=0
+       z=z1
+    end if
+    if(realpart(z)<100.0_rp) then
+       n=n+100
+       z=z+100.0_rp
+    end if
+    d=a(7)/z
+    do i=6,1,-1
+       d=a(i)/(d+z)
+    end do
+    r=z-(z-0.5_rp)*log(z)-log_pi2_2
+    zm_lgamma=d-r ! log gamma(z+n) = log (...)gamma(z)
+    if(n/=0) zm_lgamma=zm_lgamma-log(redfac())
+  contains
+    complex(cp) function redfac()
+      integer j
+      redfac=z1
+      do j=1,n-1
+         redfac=redfac*(z1+real(j,kind=rp))
+      end do
+    end function redfac
+  end function zm_lgamma
+  
+  complex(cp) function zm_gamma(z1)
+    complex(cp),intent(in)::z1
+    zm_gamma=exp(zm_lgamma(z1))
+  end function zm_gamma
 
 end module zmath
