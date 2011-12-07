47df0ed41449b3f1cfc5cc80f5f447eb4a5311d0
elseifkk <elseifkk@a4474769-b17b-4e2e-aef2-be6ca77db40a>
2011-12-07 07:01:23 +0000

    bug fix

--- fzcal.f90
+++ fzcal.f90
@@ -11,12 +11,8 @@ program fzcal
   integer istat
   integer ka
   integer cid
-  real(rp):: test=123.0
-
   
   p=init_rpnc()
-
-  istat=add_par_by_reference(rpnc%pars,"test",loc(test),.true.,PK_REAL)
 
   main: do
      write(*,10) "> "
