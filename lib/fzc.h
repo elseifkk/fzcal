48d68a5e1afcfc6f0bbd8e8568fb0559ed62f93c
elseifkk <elseifkk@a4474769-b17b-4e2e-aef2-be6ca77db40a>
2012-06-13 01:15:29 +0000

    bug fix for .fix mode and fzc.h update for compatibility.

--- lib/fzc.h
+++ lib/fzc.h
@@ -44,9 +44,8 @@ extern "C"
   size_t  __fzc_MOD_fzc_init ( void );
   size_t  __fzc_MOD_fzc_cp ( const size_t );
   void    __fzc_MOD_fzc_uinit ( const size_t );
-  void    __fzc_MOD_fzc_set_opt ( const size_t, const int );
-  void    __fzc_MOD_fzc_cle_opt ( const size_t, const int );
-  int     __fzc_MOD_fzc_proc_com ( const size_t, const size_t );
+  void    __fzc_MOD_fzc_set_mode ( const size_t, const int );
+  void    __fzc_MOD_fzc_cle_mode ( const size_t, const int );
   int     __fzc_MOD_fzc_set_formula ( const size_t, const size_t );
   int     __fzc_MOD_fzc_eval ( const size_t );
   void    __fzc_MOD_fzc_get_str_ans ( const size_t, const size_t );
@@ -60,14 +59,17 @@ extern "C"
 #define fzc_init        __fzc_MOD_fzc_init
 #define fzc_uinit       __fzc_MOD_fzc_uinit
 #define fzc_cp          __fzc_MOD_fzc_cp
-#define fzc_set_opt     __fzc_MOD_fzc_set_opt
-#define fzc_cle_opt     __fzc_MOD_fzc_cle_opt
-#define fzc_proc_com    __fzc_MOD_fzc_proc_com
+#define fzc_set_mode     __fzc_MOD_fzc_set_mode
+#define fzc_cle_mode     __fzc_MOD_fzc_cle_mode
 #define fzc_set_formula __fzc_MOD_fzc_set_formula
 #define fzc_eval        __fzc_MOD_fzc_eval
 #define fzc_regpar      __fzc_MOD_fzc_regist_parameter
 #define fzc_get_strans  __fzc_MOD_fzc_get_str_ans
 #define fzc_get_ans     __fzc_MOD_fzc_get_ans
 #define fzc_cle_dat     __fzc_MOD_fzc_cle_dat
+
+// obsolete 
+#define fzc_set_opt     __fzc_MOD_fzc_set_mode
+#define fzc_cle_opt     __fzc_MOD_fzc_cle_mode
 
 #endif 
